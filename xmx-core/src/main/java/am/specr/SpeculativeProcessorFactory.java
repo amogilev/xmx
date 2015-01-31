4599978350e5265d1529bed7228d70937c98fa1c
Andrey Mogilev <amogilev@gmail.com>
2015-01-31 19:48:37 +0600

    SpeculativeProcessorsFactory improvement - traverseParents flag

--- xmx-core/src/main/java/am/specr/SpeculativeProcessorFactory.java
+++ xmx-core/src/main/java/am/specr/SpeculativeProcessorFactory.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.WeakHashMap;
 
 /**
@@ -24,14 +23,73 @@ import java.util.WeakHashMap;
 public class SpeculativeProcessorFactory<P> {
 	
 	/**
+	 * Description of the registered processors.
+	 */
+	private static class ProcessorInfo {
+		
+		/** The processor class name */
+		String processorClassName;
+		
+		/** The names of all required classes to be searched in the target class loader */
+		String[] requiredClassesNames;
+		
+		/** 
+		 * Whether the target class loader may be one of the parents of the class loader
+		 * of the processed object, or only that loader itself.
+		 */
+		boolean traverseParents;
+		
+		public ProcessorInfo(String processorClassName,
+				String[] requiredClassesNames, boolean traverseParents) {
+			this.processorClassName = processorClassName;
+			this.requiredClassesNames = requiredClassesNames;
+			this.traverseParents = traverseParents;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime
+					* result
+					+ ((processorClassName == null) ? 0 : processorClassName
+							.hashCode());
+			result = prime * result + Arrays.hashCode(requiredClassesNames);
+			result = prime * result + (traverseParents ? 1231 : 1237);
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ProcessorInfo other = (ProcessorInfo) obj;
+			if (processorClassName == null) {
+				if (other.processorClassName != null)
+					return false;
+			} else if (!processorClassName.equals(other.processorClassName))
+				return false;
+			if (!Arrays.equals(requiredClassesNames, other.requiredClassesNames))
+				return false;
+			if (traverseParents != other.traverseParents)
+				return false;
+			return true;
+		}
+	}
+	
+	/**
 	 * Base interface or abstract class of processors managed by this factory.
 	 */
 	private Class<P> abstractProcessorClass;
 	
 	/**
-	 * All registered processors and their required classes.
+	 * Information about all registered processors, mapped by the processor class name.
 	 */
-	private Map<String, String[]> registeredProcesors = new HashMap<>();
+	private Map<String, ProcessorInfo> registeredProcesors = new HashMap<>();
 	
 	/**
 	 * Cached processors for known class loaders. 
@@ -77,15 +135,56 @@ public class SpeculativeProcessorFactory<P> {
 	 * @param requiredClassesNames names of all dependency classes of the processor class
 	 */
 	public void registerProcessor(String processorClassName, String...requiredClassesNames) {
-		String[] oldValue = registeredProcesors.get(processorClassName);
-		if (oldValue != null && !Arrays.equals(oldValue, requiredClassesNames)) {
-			throw new IllegalStateException("Speculative processor class \"" + processorClassName + 
-					"\" is already registered with another required classes");
-		}
-		
-		registeredProcesors.put(processorClassName, requiredClassesNames);
-		processorsByClassLoaderCache.clear(); // invalidate cache
+		registerProcessor(false, processorClassName, requiredClassesNames);
 	}
+	
+	/**
+	 * Registers the processor class name, which shall be used for processing objects
+	 * only when all of the required classes are available in the <i>target</i> class 
+	 * loader, which may be the class loader of the processed object, or, if 
+	 * <b>traverseParents</b> flag is set, its closest parent where the required
+	 * classes are available.
+	 * <p/>
+	 * This flag is useful for class loaders which shadows some classes available in
+	 * parents, like Jetty's WebAppClassLoader.
+	 * <p/>
+	 * <strong>The processor class MUST NOT be loaded by any public class loader anywhere
+	 * except by the factory.</strong> In particular, the processor class name needs to be passed 
+	 * as pure string literal, rather than as <code><strike>MyProcessor.class.getName()</strike></code>
+	 * <p/>
+	 * The processor class is supposed to be loaded by some class loader where all required classes,
+	 * so only two cases are supported: 
+	 * <ul>
+	 *  <li> the processor class shall be available in the target class loader,
+	 *  	e.g. added to the system class path; or
+	 *  <li> the .class files shall be available as resources for the factory's class loader;
+	 *  	then a new synthetic class loader is created with the target loader as a parent,
+	 *      and the processor class file added as resource. <b>NOTE</b>In this case, no 
+	 *      Ahead-of-Time VMs are supported.
+	 * </ul>
+	 * <p/>
+	 * The processor class MUST be a child of the base processor class specified in the 
+	 * factory's constructor. This is not checked during the registration, but may cause
+	 * further {@link ClassCastException}. Also, it MUST have no-args public constructor.
+	 *  
+	 * @param processorClassName the name of the processor class to be registered, not yet
+	 * 	loaded by any public class loader
+	 * @param requiredClassesNames names of all dependency classes of the processor class
+	 * @param traverseParents if {@code true}, not only the processed object's class loader,
+	 *    but also its parents may be the target class loader
+	 */
+	public void registerProcessor(boolean traverseParents, String processorClassName, String...requiredClassesNames) {
+		ProcessorInfo pi = new ProcessorInfo(processorClassName, requiredClassesNames, traverseParents);
+		ProcessorInfo oldValue = registeredProcesors.get(processorClassName);
+		if (oldValue == null) {
+			registeredProcesors.put(processorClassName, pi);
+			processorsByClassLoaderCache.clear(); // invalidate cache
+		} else if (oldValue.equals(pi)) {
+			throw new IllegalStateException("Speculative processor class \"" + processorClassName + 
+					"\" is already registered with another properties");
+		}
+	}
+	
 	
 	/**
 	 * Returns instances of all processor classes which have all their dependencies
@@ -118,7 +217,7 @@ public class SpeculativeProcessorFactory<P> {
 	
 	///// IMPLEMENTATION FOLLOWS ////
 	
-	protected List<P> getProcessorWith(ClassLoader classLoader) {
+	private List<P> getProcessorWith(ClassLoader classLoader) {
 		List<P> cachedList = processorsByClassLoaderCache.get(classLoader);
 		if (cachedList != null) {
 			return cachedList;
@@ -127,36 +226,70 @@ public class SpeculativeProcessorFactory<P> {
 		// new class loader, or after invalidation
 		List<P> processors = new ArrayList<>(2); // most common is 1 per loader
 		
-		for (Entry<String, String[]> entry : registeredProcesors.entrySet()) {
-			String processorClassName = entry.getKey();
-			String[] requiredClassNames = entry.getValue();
+		for (ProcessorInfo pi : registeredProcesors.values()) {
 			
-			if (availableIn(classLoader, requiredClassNames)) {
+			ClassLoader targetCL = findTargetClassLoader(classLoader, pi.traverseParents, pi.requiredClassesNames);
+			if (targetCL != null) {
 				
 				// dependencies are available, try initialize the processor
 				
-				P processor = tryCreateProcessor(classLoader, processorClassName);
+				P processor = tryCreateProcessor(targetCL, pi.processorClassName);
 				if (processor != null) {
 					processors.add(processor);
 				}
 			}
 		}
+		
+		// always use initial class loader (of processed object) as a key, as targets may
+		// be different (if there are several processors)
 		processorsByClassLoaderCache.put(classLoader, processors);
 		
 		return processors;
 	}
 
-	private P tryCreateProcessor(ClassLoader classLoader, String processorClassName) {
+	/**
+	 * Finds the target class loader, i.e. the class loader where all the required classes
+	 * are available.
+	 * <p/>
+	 * If traverseParents is {@code false}, the only the class loader of the processed objects
+	 * is checked. Otherwise, all its parents are checked as well.
+	 *  
+	 * @param processedObjectClassLoader the class loader to start the check
+	 * @param traverseParents whether to check the parents of the starting class loader
+	 * @param requiredClassesNames the name of the classes which shall be available in the
+	 * 		target class loader
+	 * 
+	 * @return the found target class loader, or {@code null}
+	 */
+	private ClassLoader findTargetClassLoader(ClassLoader processedObjectClassLoader,
+			boolean traverseParents, String[] requiredClassesNames) {
+		
+		ClassLoader targetCL = processedObjectClassLoader;
+		while (targetCL != null) {
+			if (availableIn(targetCL, requiredClassesNames)) {
+				// found
+				return targetCL;
+			}
+			if (traverseParents) {
+				targetCL = targetCL.getParent();
+			} else {
+				break;
+			}
+		}
+		return null;
+	}
+
+	private P tryCreateProcessor(ClassLoader targetClassLoader, String processorClassName) {
 		Class<? extends P> processorClass;
 		try {
-			ClassLoader extendedClassLoader = new ExtendedClassLoader(classLoader, 
+			ClassLoader extendedClassLoader = new ExtendedClassLoader(targetClassLoader, 
 					getClass().getClassLoader(), abstractProcessorClass);
 			processorClass = Class.forName(processorClassName, true, extendedClassLoader)
-					.asSubclass(abstractProcessorClass);;
+					.asSubclass(abstractProcessorClass);
 		} catch (ClassNotFoundException e) {
 			System.err.println("Processor class is not available to either class loader of processed object"
 					+ "  and class loader of the factory: class=" + processorClassName + 
-					", beanLoader=" + classLoader + ", factoryLoader=" + getClass().getClassLoader());
+					", targetLoader=" + targetClassLoader + ", factoryLoader=" + getClass().getClassLoader());
 			return null;
 		} catch (ClassCastException e) {
 			System.err.println("Processor class MUST be subclass of base processor class: "
@@ -164,7 +297,7 @@ public class SpeculativeProcessorFactory<P> {
 			return null;
 		} catch (Error e) {
 			System.err.println("Failed to initialize processor class: "
-					+ "class=" + processorClassName + ", loader=" + classLoader);
+					+ "class=" + processorClassName + ", loader=" + targetClassLoader);
 			e.printStackTrace();
 			return null;
 		}
@@ -191,12 +324,12 @@ public class SpeculativeProcessorFactory<P> {
 	}
 
 	private boolean availableIn(ClassLoader loader, String...classNames) {
-		try {
-			for (String className : classNames) {
+		for (String className : classNames) {
+			try {
 				loader.loadClass(className);
+			} catch (ClassNotFoundException e) {
+				return false;
 			}
-		} catch (ClassNotFoundException e) {
-			return false;
 		}
 		
 		return true;
