75671e9ba6c1dad262327a4d0e184c82be051e3f
Andrey Mogilev <amogilev@gmail.com>
2017-07-25 00:28:37 +0700

    temporarily disable toJson() in case of errors
    Used to prevent slow-down due to frequent multiple StackOverflowErrors

--- xmx-core/src/main/java/am/xmx/service/MapperService.java
+++ xmx-core/src/main/java/am/xmx/service/MapperService.java
@@ -6,19 +6,35 @@ import org.slf4j.LoggerFactory;
 
 public class MapperService implements IMapperService {
 
-	private final static Logger logger = LoggerFactory.getLogger(MapperService.class);
+	private static final Logger logger = LoggerFactory.getLogger(MapperService.class);
 	private static final YaGson jsonMapper = new YaGson();
+	private static final String NOT_AVAILABLE = "N/A";
+	private static final long JSON_DISABLE_ON_ERROR_SECONDS = 30;
+
+	private volatile long toJsonDisabledUntilTime = 0;
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public String safeToJson(Object obj) {
+		if (toJsonDisabledUntilTime > 0) {
+			if (toJsonDisabledUntilTime > System.currentTimeMillis()) {
+				// still disabled
+				return NOT_AVAILABLE;
+			} else {
+				// clear flag for faster further checks. NOTE: data race is possible, but we do not care much
+				//   about synchronization here
+				toJsonDisabledUntilTime = 0;
+			}
+		}
 		try {
 			return jsonMapper.toJson(obj, Object.class);
 		} catch (Throwable e) {
 			logger.warn("toJson() failed for an instance of {}", obj.getClass(), e);
-			return "";
+
+			toJsonDisabledUntilTime = System.currentTimeMillis() + 1000 * JSON_DISABLE_ON_ERROR_SECONDS;
+			return NOT_AVAILABLE;
 		}
 	}
 
